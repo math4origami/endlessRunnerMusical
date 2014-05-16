a30332ab556a85baefe1c8816a34bd50e774f0ac
Kevin Tan <kevin@Kevin-Tan.local>
2014-05-16 00:16:56 -0700

    Updating PlayerController to keep track of the player's lives

--- EndlessRunnerMusical/Assets/Scripts/PlayerController.cs
+++ EndlessRunnerMusical/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public int lives;
+	private int originalNumLives;
+	private int currentNumLives;
+
 	// Use this for initialization
 	void Start () {
-	
+		originalNumLives = lives;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void resetPlayer() {
+		currentNumLives = originalNumLives;
+	}
+
+	public void deductLife() {
+		--currentNumLives;
+
+		if (currentNumLives <= 0) {
+			currentNumLives = 0;
+		}
+	}
+
+	public bool isAlive() {
+		return (currentNumLives > 0);
 	}
 }
