6d8049f986bf09f45befdca163a602ea6d34ba79
Kevin Tan <kevin@Kevin-Tan.local>
2014-05-15 22:23:36 -0700

    Adding new scenes to represent UI screens, and add normal button prefab that has awesome default size for a button to be created

---
+++ EndlessRunnerMusical/Assets/Scripts/GUIButtonMake.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIButtonMake : MonoBehaviour {
+
+	public GUISkin customSkin;
+	public string buttonText;
+	public string objectName;
+	private GameObject actualObject;
+	public string scriptFunction;
+	public int posX;
+	public int posY;
+	public int sizeX;
+	public int sizeY;
+
+	// Use this for initialization
+	void Start () {
+		actualObject = GameObject.Find(objectName);
+	}
+
+	void OnGUI() {
+		GUI.skin = customSkin;
+
+		if (GUI.Button(new Rect(posX, posY, sizeX, sizeY), buttonText)) {
+			if (actualObject) {
+				//Do something
+				actualObject.SendMessage(scriptFunction);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ EndlessRunnerMusical/Assets/Scripts/WinLevelScreenController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinLevelScreenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void startLevel () {
+		Application.LoadLevel("Running");
+	}
+}
