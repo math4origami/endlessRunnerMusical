f68fa4314222daf276ed57e50aa6ea8c6eec3b19
Kevin Tan <kevin@Kevin-Tan.local>
2014-05-15 23:15:23 -0700

    missing meta files

---
+++ EndlessRunnerMusical/Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void startLevel() {
+		Application.LoadLevel("Running");
+	}
+}
