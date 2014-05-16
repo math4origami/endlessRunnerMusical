741f992779a0c4286ac667ec29f8ebd2451500c3
Kevin Tan <kevin@Kevin-Tan.local>
2014-05-16 02:25:03 -0700

    Add Heart prefab and update PlayerController to control it

--- EndlessRunnerMusical/Assets/Scripts/PlayerController.cs
+++ EndlessRunnerMusical/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,17 @@ public class PlayerController : MonoBehaviour {
 	public int lives;
 	private int originalNumLives;
 	private int currentNumLives;
+	public int CurrentNumLives {
+		get {return currentNumLives;}
+	}
+	public GameObject heart1;
+	public GameObject heart2;
+	public GameObject heart3;
 
 	// Use this for initialization
 	void Start () {
 		originalNumLives = lives;
+		resetPlayer();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +26,31 @@ public class PlayerController : MonoBehaviour {
 
 	public void resetPlayer() {
 		currentNumLives = originalNumLives;
+
+		updateHearts();
+	}
+
+	private void updateHearts() {
+		Debug.Log ("Who is you?");
+		heart1.guiTexture.enabled = false;
+		heart2.guiTexture.enabled = false;
+		heart3.guiTexture.enabled = false;
+		for (int i=0; i<currentNumLives; ++i) {
+			switch (i) {
+				case 0: {
+					heart1.guiTexture.enabled = true;
+					break;
+				}
+				case 1: {
+					heart2.guiTexture.enabled = true;
+					break;
+				}
+				case 2: {
+					heart3.guiTexture.enabled = true;
+					break;
+				}
+			}
+		}
 	}
 
 	public void deductLife() {
@@ -27,6 +59,8 @@ public class PlayerController : MonoBehaviour {
 		if (currentNumLives <= 0) {
 			currentNumLives = 0;
 		}
+
+		updateHearts();
 	}
 
 	public bool isAlive() {
