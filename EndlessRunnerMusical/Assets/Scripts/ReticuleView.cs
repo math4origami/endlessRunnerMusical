da09417b0a17c51f0d8e0acf7ed125c2ed534da3
Russell Chou <russell@storm8.com>
2014-05-16 06:55:00 -0700

    more reticule

--- EndlessRunnerMusical/Assets/Scripts/ReticuleView.cs
+++ EndlessRunnerMusical/Assets/Scripts/ReticuleView.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class ReticuleView : MonoBehaviour {
 
+	public float baseScale;
+	public float maxScale;
 	public float baseAlpha;
 	public float maxAlpha;
 	public float alphaDistance;
+	public float rotationSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -20,16 +23,23 @@ public class ReticuleView : MonoBehaviour {
 		ScoreNote note = score.getNote(gc.gameTime(), true);
 		float alpha = baseAlpha;
 		Color color = GetComponent<SpriteRenderer>().color;
+		float scale = baseScale;
 
 		if (note != null) {
 			float interval = note.scriptNote.interval(gc.gameTime());
-			
+
 			if (interval < alphaDistance) {
-				alpha += (maxAlpha - baseAlpha) * (alphaDistance - interval) / alphaDistance;
+				float interpolate = (alphaDistance - interval) / alphaDistance;
+				alpha += (maxAlpha - baseAlpha) * interpolate;
+				scale += (maxScale - baseScale) * interpolate;
 			}
 		}
 
 		color.a = alpha;
 		GetComponent<SpriteRenderer>().color = color;
+		transform.localScale = Vector3.one * scale;
+
+		float angle = gc.gameTime() * rotationSpeed;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 }
